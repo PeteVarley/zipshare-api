98d5cffdbb8ce59a2ed65cf5edf7ad214d2292fd
Varley <petevarley@gmail.com>
2015-02-18 00:49:20 -0500

    -    get "/health_asdfasdfasdfasdfcheck", format: :json +    get "/health_check", format: :json
    manual test proved that travisci is doing it's job

--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Health check" do
   it "returns 200 when database is available" do
-    get "/health_asdfasdfasdfasdfcheck", format: :json
+    get "/health_check", format: :json
 
     expect(response).to be_ok
   end
