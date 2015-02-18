12b82521d6736bb487faebfe11dce1089acf45dd
Varley <petevarley@gmail.com>
2015-02-18 00:44:26 -0500

    manually testing process

--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Health check" do
   it "returns 200 when database is available" do
-    get "/health_check", format: :json
+    get "/health_asdfasdfasdfasdfcheck", format: :json
 
     expect(response).to be_ok
   end
